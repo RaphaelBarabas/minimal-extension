d680227a2410c6d1e81e873dab32479f4d8cc196
Raphael Barabas <r.barabas@sap.com>
2024-01-26 17:18:25 +0100

    Add helper to get activity from cloud

--- helpers.js
+++ helpers.js
@@ -17,3 +17,51 @@ function initializeRefreshTokenStrategy(shellSdk, auth) {
   sessionStorage.setItem('token', auth.access_token);
   setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
 }
+
+// 
+// Request context with activity ID to return the activities data
+//
+function getActivity(cloudHost, account, company, activityId) {
+  
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-Client-ID': 'fsm-extension-sample',
+    'X-Client-Version': '1.0.0',
+    'Authorization': `bearer ${sessionStorage.getItem('token')}`,
+  };
+
+  return new Promise(resolve => {
+
+    // Fetch Activity object
+    fetch(`https://${cloudHost}/api/data/v4/Activity/${activityId}?dtos=Activity.42&account=${account}&company=${company}`, {
+      headers
+      })
+      .then(response => response.json())
+      .then(function(json) {
+        resolve(json.data[0].activity);
+      });
+  });
+}
+
+//          // Fetch all ServiceContractEquipment
+//          fetch(`https://${cloudHost}/api/data/v4/ServiceContractEquipment?dtos=ServiceContractEquipment.12&account=${account}&company=${company}`, {
+//            headers
+//            })
+//              .then(response => response.json())
+//              .then(function(json) {
+//
+//                const serviceContractEquipment = json.data.find(contract => contract.serviceContractEquipment.equipment === activity.equipment);
+//                if (!serviceContractEquipment) {
+//                  resolve(null);
+//                } else {
+//                  fetch(`https://${cloudHost}/api/data/v4/ServiceContract/${serviceContractEquipment.serviceContractEquipment.serviceContract}?dtos=ServiceContract.13&account=${account}&company=${company}`, {
+//                    headers
+//                    })
+//                      .then(response => response.json())
+//                      .then(function(json) {
+//                        resolve(json.data[0].serviceContract);
+//                      });
+//                }
+//
+//              });
+//
